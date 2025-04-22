6e40ede68cd91aee4747917de0d508498677d233
Addan Abdullah <addanabdullahsiddiqui@gmail.com>
2025-04-22 23:39:30 +0500

    Profile Controller Integration?

--- lib/pages/student_home_page.dart
+++ lib/pages/student_home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project/controllers/auth_controller.dart';
+import 'package:project/controllers/profile_controller.dart';
 import 'package:project/pages/profile_page.dart';
 
 class StudentHomePage extends StatelessWidget {
   final AuthController authController = Get.find<AuthController>();
+  final ProfileController profileController =
+      Get.put<ProfileController>(ProfileController());
   StudentHomePage({super.key});
 
   @override
@@ -57,45 +60,98 @@ class StudentHomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Get.theme.colorScheme.tertiaryContainer,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Column(
-                    children: List.generate(
-                      3,
-                      (index) => Text("Item $index"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Obx(
+              () => Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 8,
+                ),
+                child: Text.rich(
+                  TextSpan(text: "Hello, ", children: [
+                    TextSpan(
+                      text: profileController.name ?? "User",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Get.theme.colorScheme.primary,
+                      ),
                     ),
-                  ),
-                  Column(
-                    children: List.generate(
-                      3,
-                      (index) => Text("Item $index"),
-                    ),
-                  ),
-                ],
+                  ]),
+                  // "Hello, ${profileController.name}",
+                  style: Get.textTheme.headlineLarge,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) => ListTile(
-                title: Text("Item $index"),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Get.theme.colorScheme.primaryContainer,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Column(
+                      children: List.generate(
+                        3,
+                        (index) => Text(
+                          "Item $index",
+                          style: TextStyle(
+                            color: Get.theme.colorScheme.onPrimaryContainer,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: List.generate(
+                        3,
+                        (index) => Text(
+                          "Item $index",
+                          style: TextStyle(
+                            color: Get.theme.colorScheme.onPrimaryContainer,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 20,
+                // itemBuilder: (context, index) => ListTile(
+                //   title: Text("Item $index"),
+                // ),
+                itemBuilder: (context, index) => Card(
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    color: Get.theme.colorScheme.primaryContainer,
+                    child: Center(
+                      child: Text(
+                        "Item $index",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Get.theme.colorScheme.onPrimaryContainer,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
