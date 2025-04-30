00de2b5aa7ffeb7e33f6791446ea26cae10dc87c
Addan Abdullah <addanabdullahsiddiqui@gmail.com>
2025-04-30 10:20:18 +0500

    Refactor StudentResultsPage layout; improve UI structure and enhance CGPA and course display logic

--- lib/pages/student_results_page.dart
+++ lib/pages/student_results_page.dart
@@ -13,7 +13,6 @@ class StudentResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// TODO: Style the texts better
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -25,88 +24,95 @@ class StudentResultsPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          ItemContainer(
-            marginX: 8,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "CGPA",
-                  style: Get.textTheme.bodyLarge!.copyWith(
-                    color: Get.theme.colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Obx(
-                  () => Text(
-                    stateController.cgpa.toStringAsFixed(2),
-                    style: Get.theme.textTheme.headlineSmall!.copyWith(
-                      color: Get.theme.colorScheme.onPrimaryContainer,
-                      fontWeight: FontWeight.bold,
+      body: Obx(
+        () => Skeletonizer(
+          enabled: stateController.isLoading,
+          child: Column(
+            children: [
+              ItemContainer(
+                marginX: 8,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "CGPA",
+                      style: Get.textTheme.bodyLarge!.copyWith(
+                        color: Get.theme.colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
+                    Obx(
+                      () => Text(
+                        stateController.cgpa.toStringAsFixed(2),
+                        style: Get.theme.textTheme.headlineSmall!.copyWith(
+                          color: Get.theme.colorScheme.onPrimaryContainer,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          Text(
-            "Courses",
-            style: Get.textTheme.headlineSmall!.copyWith(
-              color: Get.theme.colorScheme.primary,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Expanded(
-            child: Obx(() => Skeletonizer(
-                  enabled: stateController.isLoading,
-                  child: ListView.builder(
-                    itemCount: stateController.results.length,
-                    itemBuilder: (context, index) {
-                      final result = stateController.results[index];
-                      return ItemContainer(
-                        marginX: 8,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              result["title"].toString(),
-                              style: Get.textTheme.bodyLarge!.copyWith(
-                                color: Get.theme.colorScheme.primary,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Container(
-                              width: 30,
-                              height: 30,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                color: Get.theme.colorScheme.tertiaryContainer,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  result["grade"].toString(),
+              ),
+              Text(
+                "Courses",
+                style: Get.textTheme.headlineSmall!.copyWith(
+                  color: Get.theme.colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Expanded(
+                child: Obx(() => Skeletonizer(
+                      enabled: stateController.isLoading,
+                      child: ListView.builder(
+                        itemCount: stateController.results.length,
+                        itemBuilder: (context, index) {
+                          final result = stateController.results[index];
+                          return ItemContainer(
+                            marginX: 8,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  result["title"].toString(),
                                   style: Get.textTheme.bodyLarge!.copyWith(
+                                    color: Get.theme.colorScheme.primary,
                                     fontWeight: FontWeight.bold,
-                                    color: {
-                                          "A+": Get.theme.colorScheme.primary,
-                                          "F": Get.theme.colorScheme.error
-                                        }[result["grade"]] ??
-                                        Get.theme.colorScheme
-                                            .onSecondaryContainer,
                                   ),
                                 ),
-                              ),
+                                Container(
+                                  width: 30,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    color:
+                                        Get.theme.colorScheme.tertiaryContainer,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      result["grade"].toString(),
+                                      style: Get.textTheme.bodyLarge!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                        color: {
+                                              "A+":
+                                                  Get.theme.colorScheme.primary,
+                                              "F": Get.theme.colorScheme.error
+                                            }[result["grade"]] ??
+                                            Get.theme.colorScheme
+                                                .onSecondaryContainer,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                )),
+                          );
+                        },
+                      ),
+                    )),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
